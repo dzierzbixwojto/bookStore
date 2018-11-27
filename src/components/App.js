36c70986125e99d15f7bdae57dadb7ad7473e8c1
Wojciech Dzierzba <dzierzbix@gmail.com>
2018-11-27 01:27:40 +0100

    First commit!

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Book from './Book'
+
+class App extends React.Component{
+    render(){
+        return <div className="App"> 
+        <h1>Hello Wojtek</h1>
+        <Book />
+        </div>
+    }
+}
+export default App;
---
+++ src/components/Book.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+class Book extends React.Component{
+    render(){
+        return <p>Here i have book</p>
+    }
+}
+export default Book;
